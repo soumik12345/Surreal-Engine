1c91f02a7bc84a632bbf8dcfd752ee2cfd8f3556
soumik12345 <19soumik.rakshit96@gmail.com>
2020-07-01 22:21:46 +0530

    Fixed a small declspec (Thanks Anurup ;))

--- SurrealEngine/src/Surreal/Core.h
+++ SurrealEngine/src/Surreal/Core.h
@@ -5,7 +5,7 @@
 	#ifdef SR_BUILD_DLL
 		#define SURREAL_API __declspec(dllexport)
 	#else
-		#define SURREAL_API __declspec(dllexport)
+		#define SURREAL_API __declspec(dllimport)
 	#endif // SR_BUILD_DLL
 #else
 	#error Surreal Engine is supported by Windows Only
